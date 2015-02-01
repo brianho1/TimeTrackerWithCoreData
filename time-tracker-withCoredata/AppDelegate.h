7ebc4f201c942d68b829b8b784dbd323c1ffcdc1
kumok <bhh@byu.edu>
2015-02-01 16:31:20 -0700

    Initial Commit

---
+++ time-tracker-withCoredata/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  time-tracker-withCoredata
+//
+//  Created by Duc Ho on 2/1/15.
+//  Copyright (c) 2015 self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ time-tracker-withCoredata/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  time-tracker-withCoredata
+//
+//  Created by Duc Ho on 2/1/15.
+//  Copyright (c) 2015 self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
